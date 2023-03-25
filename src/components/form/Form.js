fa08f045b456aec19184cd20b09a35cde02ad0a3
Tobias <113835194+optobimus@users.noreply.github.com>
2023-03-25 21:21:56 +0100

    Refactor to functional component (167 lines to 139)

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 
 import "../../styles/form.css"
 
@@ -8,31 +8,22 @@ import Educational from "./Educational";
 import Formtitle from "./Formtitle";
 import Multiple from "./Multiple";
 
-class Form extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      activeExp: null,
-      activeEdu: null,
-    }
-    this.handleActiveExperience = this.handleActiveExperience.bind(this);
-    this.handleActiveEducation = this.handleActiveEducation.bind(this);
+const Form = ({ cv, onChangePersonal, onChangeExperience, onChangeEducation, onAddExperience, onAddEducation, onDeleteExperience, onDeleteEducation, onLoadExample, onReset, onPDF }) => {
 
-    this.handleDeleteExperienceClass = this.handleDeleteExperienceClass.bind(this);
-    this.handleDeleteEducationClass = this.handleDeleteEducationClass.bind(this);
-  }
+  const [activeExp, setActiveExp] = useState(null);
+  const [activeEdu, setActiveEdu] = useState(null);
 
-  handleActiveExperience(e) {
+  const handleActiveExperience = (e) => {
     const activeExp = e.dataset.id;
-    this.setState({ activeExp });
+    setActiveExp(activeExp);
   }
 
-  handleActiveEducation(e) {
+  const handleActiveEducation = (e) => {
     const activeEdu = e.dataset.id;
-    this.setState({ activeEdu });
+    setActiveEdu(activeEdu);
   }
 
-  handleDeleteExperienceClass(id) {
+  const handleDeleteExperienceClass = (id) => {
     let position = 0;
     const experienceBtns = Array.from(document.querySelectorAll(".experience-header .nav-button"));
     experienceBtns.forEach((item) => {
@@ -41,14 +32,14 @@ class Form extends Component {
       }
     })
     if (position === 0) {
-      this.setState({ activeExp: experienceBtns[1].dataset.id })
+      setActiveExp(experienceBtns[1].dataset.id);
     } else {
-      this.setState({ activeExp: experienceBtns[position - 1].dataset.id})
+      setActiveExp(experienceBtns[position - 1].dataset.id)
     }
-    this.props.onDeleteExperience(id);
+    onDeleteExperience(id);
   }
 
-  handleDeleteEducationClass(id) {
+  const handleDeleteEducationClass = (id) => {
     let position = 0;
     const educationBtns = Array.from(document.querySelectorAll(".education-header .nav-button"));
     educationBtns.forEach((item) => {
@@ -57,111 +48,95 @@ class Form extends Component {
       }
     })
     if (position === 0) {
-      this.setState({ activeEdu: educationBtns[1].dataset.id })
+      setActiveEdu(educationBtns[1].dataset.id)
     } else {
-      this.setState({ activeEdu: educationBtns[position - 1].dataset.id})
+      setActiveEdu(educationBtns[position - 1].dataset.id)
     }
-    this.props.onDeleteEducation(id);
+    onDeleteEducation(id);
   }
 
-  componentDidMount() {
-    this.setActiveItems();
-  }
+  useEffect(() => {
+    setActiveItems();
+  }, []);
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.activeExp !== this.state.activeExp || prevState.activeEdu !== this.state.activeEdu) {
-      this.setActiveItems();
-    }
-  }
+  useEffect(() => {
+    setActiveItems();
+  }, [activeEdu, activeExp]);
 
-  setActiveItems() {
+  const setActiveItems = () => {
     const experienceBtns = document.querySelectorAll(".experience-header .nav-button");
     const educationBtns = document.querySelectorAll(".education-header .nav-button");
 
     if (experienceBtns.length === 1) {
-      this.setState({ activeExp: experienceBtns[0].dataset.id });
+      setActiveExp(experienceBtns[0].dataset.id);
     }
 
     if (educationBtns.length === 1) {
-      this.setState({ activeEdu: educationBtns[0].dataset.id });
+      setActiveEdu(educationBtns[0].dataset.id);
     }
   }
 
-  render() {
-    const { 
-      cv, 
-      onChangePersonal, 
-      onChangeExperience, 
-      onChangeEducation, 
-      onAddExperience, 
-      onAddEducation, 
-      onLoadExample, 
-      onReset, 
-      onPDF 
-    } = this.props;
-    const { activeExp, activeEdu } = this.state;
+  const experienceItems = cv.experience.map((experienceItem) => {
+      if (activeExp === experienceItem.id) {
+          return <Practical 
+            key={experienceItem.id}
+            experienceInfo={experienceItem}
+            onChange={onChangeExperience}
+            id={experienceItem.id}
+          />
+        }
+    
+    return null;
+  });
 
+  const educationItems = cv.education.map((educationItem) => {
+    if (activeEdu === educationItem.id) {
+      return <Educational 
+        key={educationItem.id}
+        educationalInfo={educationItem}
+        onChange={onChangeEducation}
+        id={educationItem.id}
+      />
+    }
+    return null;
 
-    const experienceItems = cv.experience.map((experienceItem) => {
-        if (activeExp === experienceItem.id) {
-            return <Practical 
-              key={experienceItem.id}
-              experienceInfo={experienceItem}
-              onChange={onChangeExperience}
-              id={experienceItem.id}
-            />
-          }
-      
-      return null;
-    });
+  });
 
-    const educationItems = cv.education.map((educationItem) => {
-      if (activeEdu === educationItem.id) {
-        return <Educational 
-          key={educationItem.id}
-          educationalInfo={educationItem}
-          onChange={onChangeEducation}
-          id={educationItem.id}
+  return (
+    <div className="inputForm">
+      <Personal personalInfo={cv.personal} onChange={onChangePersonal}/>
+      <div className="experience-header">
+        <Formtitle 
+          title="Experience" 
+          className="experience-header-left" 
+          onClickAdd={onAddExperience} 
+          onClickDelete={handleDeleteExperienceClass}
+          id = {activeExp}
         />
-      }
-      return null;
-
-    });
-
-    return (
-      <div className="inputForm">
-        <Personal personalInfo={cv.personal} onChange={onChangePersonal}/>
-        <div className="experience-header">
-          <Formtitle 
-            title="Experience" 
-            className="experience-header-left" 
-            onClickAdd={onAddExperience} 
-            onClickDelete={this.handleDeleteExperienceClass}
-            id = {activeExp}
-          />
-          <Multiple data={cv.experience} onClick={this.handleActiveExperience} activeID={activeExp}/>
-        </div>
-        {experienceItems}
-        <div className="education-header">
-          <Formtitle 
-            title="Education" 
-            className="education-header-left" 
-            onClickAdd={onAddEducation}
-            onClickDelete={this.handleDeleteEducationClass}
-            id = {activeEdu}
-          />
-          <Multiple data={cv.education} onClick={this.handleActiveEducation} activeID={activeEdu}/>
-        </div>
-        {educationItems}
-        <div className="buttons-container">
-          <button type="button" className="pdf-button" onClick={onPDF}>Generate PDF</button>
-          <button type="button" className="example-button" onClick={onLoadExample}>Load Example</button>
-          <button type="button" className="reset-button" onClick={onReset}>Reset</button>
-        </div>
-        </div>  
-    );
-  }
+        <Multiple data={cv.experience} onClick={handleActiveExperience} activeID={activeExp}/>
+      </div>
+      {experienceItems}
+      <div className="education-header">
+        <Formtitle 
+          title="Education" 
+          className="education-header-left" 
+          onClickAdd={onAddEducation}
+          onClickDelete={handleDeleteEducationClass}
+          id = {activeEdu}
+        />
+        <Multiple data={cv.education} onClick={handleActiveEducation} activeID={activeEdu}/>
+      </div>
+      {educationItems}
+      <div className="buttons-container">
+        <button type="button" className="pdf-button" onClick={onPDF}>Generate PDF</button>
+        <button type="button" className="example-button" onClick={onLoadExample}>Load Example</button>
+        <button type="button" className="reset-button" onClick={onReset}>Reset</button>
+      </div>
+      </div>  
+  );
 }
 
-
 export default Form;
+
+
+// 167
